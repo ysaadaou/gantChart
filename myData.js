58fdc01a22b792204279350e85b3b5bf084cc89e
ysaadou <yhiasaad748@gmail.com>
2024-10-17 01:22:53 +0100

    first commit

---
+++ myData.js
@@ -0,0 +1,677 @@
+const gantData = {
+  4500012490: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-12",
+          endingDate: "2024-08-02",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-08-02",
+          endingDate: "2025-02-22",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-22",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-01-08",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-08",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013501: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-19",
+          endingDate: "2025-04-15",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-07-31",
+          endingDate: "2025-04-15",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-04-15",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-14",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-14",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500012607: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-03",
+          endingDate: "2024-08-02",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-08-02",
+          endingDate: "2024-12-04",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-12-04",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  "4500012656 / 4500012657": {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-08",
+          endingDate: "2024-08-29",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-08-29",
+          endingDate: "2025-01-08",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-08",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  4500012610: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: "2024-09-03",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-03",
+          endingDate: "2025-02-26",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-26",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-26",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  4500013344: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: "2024-09-05",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-05",
+          endingDate: "2025-03-28",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-03-28",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-03-28",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-26",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013003: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-29",
+          endingDate: "2024-09-11",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-11",
+          endingDate: "2025-06-09",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-06-09",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-06-09",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-03-28",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013060: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-29",
+          endingDate: "2024-09-11",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-11",
+          endingDate: "2025-06-02",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-06-02",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-06-02",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-06-09",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013028: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-13",
+          endingDate: "2024-09-12",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-12",
+          endingDate: "2025-02-22",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-22",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-22",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-06-02",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  45000126717: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: "2024-09-18",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-18",
+          endingDate: "2025-01-02",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-02",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-22",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013350: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-16",
+          endingDate: "2024-09-19",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-19",
+          endingDate: "2024-11-14",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-11-14",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2024-11-14",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  4500013347: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-16",
+          endingDate: "2024-09-19",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-09-19",
+          endingDate: "2025-01-16",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-16",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-01-16",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-11-14",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500007152-53/J-2023": {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2023-05-09",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        { name: "KOM", startingDate: "", endingDate: "", status: "done" },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "",
+          endingDate: "2025-06-07",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2023-10-10",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-16",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500013694/JESA/24": {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-12",
+          endingDate: "2024-10-02",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-10-02",
+          endingDate: "2024-12-11",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-12-11",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2024-12-11",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-01-02",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013749: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-26",
+          endingDate: "2024-10-07",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-10-07",
+          endingDate: "2025-01-24",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-24",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-01-24",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-12-11",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013478: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "KOM",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-24",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500013524/24": {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-10",
+          endingDate: "2024-10-10",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-10-10",
+          endingDate: "2025-02-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-07",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-07",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  "4500013627/24": {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-20",
+          endingDate: "2024-04-10",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-04-10",
+          endingDate: "2025-02-20",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-20",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-20",
+          endingDate: "2025-03-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-07",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013596: {
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-06",
+          endingDate: "2024-10-07",
+          status: "",
+        },
+        {
+          name: "KOM",
+          startingDate: "2024-10-07",
+          endingDate: "2024-12-05",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-12-05",
+          endingDate: "2025-06-07",
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "",
+          endingDate: "",
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-20",
+          endingDate: "2025-01-30",
+          status: "",
+        },
+      ],
+    },
+  },
+};
+
+export default gantData;
